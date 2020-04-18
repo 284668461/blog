45940d5bb5a03acc93f2c1a7cefecd096b5c44c4
284668461 <38812759+284668461@users.noreply.github.com>
2020-04-18 22:59:31 +0800

    '0418'

--- static/js/index.js
+++ static/js/index.js
@@ -1,6 +1,6 @@
 $(()=>{
 
-
+    //导航条 start---------------------------
     $("#sidebar").on("click",()=>{
 
         $('.m-item').toggleClass("m-mobile-hide");
@@ -8,7 +8,25 @@ $(()=>{
 
 
 
-//    登录事件
+    //切换 section
+    $("#nav a.item").click(function(){
+
+
+        let thisLabel = $(this).attr("data-section");
+
+        $(this).addClass("active").siblings().removeClass("active");
+
+        $(`section.${thisLabel}`).show(300).siblings().hide(300);
+
+
+
+
+    });
+
+    //导航条 end---------------------------
+
+
+//登录 start ---------------------------
 
     $("#loginBtn").on("click",()=>{
 
@@ -18,6 +36,10 @@ $(()=>{
 
 
     });
+
+//登录 end   ---------------------------
+
+
 
 
 
